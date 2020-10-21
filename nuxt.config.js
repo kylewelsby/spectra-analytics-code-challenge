fdba810262d39c65a4eb61122135e184d2d867c0
Kyle Welsby <kyle@mekyle.com>
2020-10-21 01:32:35 +0100

    🐙Github Pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,9 @@ export default {
 
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
+  router: {
+    base: process.env.BASE_URL || '/'
+  },
   generate: {
     fallback: '404.html',
     routes: ['/']
